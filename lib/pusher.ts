fa67d971e8d8c1c73e34c51121c6d0218903e957
Bui Tran Thien An <buitranthienan1111@gmail.com>
2024-12-14 09:41:21 +0700

    u

--- lib/pusher.ts
+++
@@ -1,9 +0,0 @@
-import Pusher from "pusher";
-
-export const pusher = new Pusher({
-  appId: "1879254",
-  key: "671afd8438e48f336089",
-  secret: "70a0c8d744eb44323ecd",
-  cluster: "ap1",
-  useTLS: true,
-});

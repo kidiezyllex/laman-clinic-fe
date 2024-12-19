96dd87d858c5da7670483508151874123df0f88d
Bui Tran Thien An <buitranthienan1111@gmail.com>
2024-12-20 01:28:06 +0700

    update 2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -16,15 +16,11 @@ const authOptions: AuthOptions = {
         }
 
         try {
-          const res = await fetch(
-            `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(credentials),
-            }
-          );
-          console.log(process.env.NEXT_PUBLIC_BACKEND_API_URL);
+          const res = await fetch(`${process.env.BACKEND_API_URL}/auth/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(credentials),
+          });
           if (!res.ok) {
             const errorDetails = await res.json();
             throw new Error(
